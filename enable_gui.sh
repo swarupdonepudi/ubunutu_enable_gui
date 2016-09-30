66abe15fb4b76041486e18f7f0412bb8de66bd31
swarup.donepudi@gmail.com <swarup.donepudi@gmail.com>
2016-09-30 15:15:38 -0500

    fixed sudo problem

--- enable_gui.sh
+++ enable_gui.sh
@@ -2,5 +2,6 @@ sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install ubuntu-desktop vnc4server gnome-panel
 vncserver
 vncserver -kill :1
-cp ./xstartup /root/.vnc/xstartup
+sudo cp ./xstartup /root/.vnc/xstartup
+sudo update-rc.d vncserver defaults
 vncserver
