cf4ae5b1243a0af56495d72185777f2b939bac02
swarup.donepudi@gmail.com <swarup.donepudi@gmail.com>
2016-09-30 15:01:30 -0500

    replaced library with gnome

--- README.md
+++ README.md
@@ -2,13 +2,13 @@ Run instructions:
 
 Install git :
 
-```
+```shell
 sudo apt-get --assume-yes install git
 ```
 
 Clone this repo :
 
-```
+```shell
 git clone https://github.com/swarup-donepudi/ubunutu_enable_gui.git
 
 cd ubunutu_enable_gui/
@@ -17,3 +17,4 @@ chmod +x enable_gui.sh
 
 ./enable_gui.sh "ubuntu" | tee -a enable_gui_execution.log
 ```
+> Note: The shell script will prompt password for VNC server couple of times
--- enable_gui.sh
+++ enable_gui.sh
@@ -1,15 +1,6 @@
-USER=$2
-sudo apt-get -y update
-sudo apt-get -y install xfce4 xfce4-goodies tightvncserver
+sudo apt-get --assume-yes update
+sudo apt-get --assume-yes install ubuntu-desktop vnc4server gnome-panel
 vncserver
 vncserver -kill :1
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-echo "#!/bin/bash\nxrdb $HOME/.Xresources\nstartxfce4 &" > ~/.vnc/xstartup
-sudo chmod +x ~/.vnc/xstartup
-sudo cp ./vncserver_service.sh /etc/init.d/vncserver
-export_user_find_string='export USER=\"user\"'
-export_user_replace_string='export USER=\"'$USER'\"'
-sudo sed  -i 's/'"$export_user_find_string"'/'"$export_user_replace_string"'/' /etc/init.d/vncserver
-sudo chmod +x /etc/init.d/vncserver
-sudo service vncserver start
-sudo update-rc.d vncserver defaults
+cp ./xstartup /root/.vnc/xstartup
+vncserver
