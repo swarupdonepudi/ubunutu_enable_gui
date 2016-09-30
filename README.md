aa7404e14feedcd4e5ad45dbd53e254cf599e460
swarup.donepudi@gmail.com <swarup.donepudi@gmail.com>
2016-09-30 10:58:00 -0500

    updated shell script and readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Run instructions:
 
-Install git
-Clone this repo : git clone <repo_url>
-Run the shell script : ./enable_gui.sh
+Install git : sudo apt-get --assume-yes install git
+Clone this repo : git clone https://github.com/swarup-donepudi/ubunutu_enable_gui.git
+cd ubunutu_enable_gui/
+chmod +x enable_gui.sh
+Run the shell script : ./enable_gui.sh "hello" "ubuntu"
--- enable_gui.sh
+++ enable_gui.sh
@@ -2,15 +2,15 @@ VNC_SERVER_PASSWORD=$1
 USER=$2
 sudo apt-get -y update
 sudo apt-get -y install xfce4 xfce4-goodies tightvncserver
-echo $VNC_SERVER_PASSWORD | vncserver
+echo "$VNC_SERVER_PASSWORD\n$VNC_SERVER_PASSWORD\n" | vncserver -S
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 echo "#!/bin/bash\nxrdb $HOME/.Xresources\nstartxfce4 &" > ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
-cp ./vncserver_service.sh /etc/init.d/vncserver
-export_user_find_string='export USER="user"'
-export_user_replace_string='export USER="$USER"'
-sed -i 's/$export_user_find_string/$export_user_replace_string/g' /etc/init.d/vncserver
+sudo cp ./vncserver_service.sh /etc/init.d/vncserver
+export_user_find_string='export USER=\"user\"'
+export_user_replace_string='export USER=\"'$USER'\"'
+sudo sed  -i 's/'"$export_user_find_string"'/'"$export_user_replace_string"'/' /etc/init.d/vncserver
 sudo chmod +x /etc/init.d/vncserver
 sudo service vncserver start
 sudo update-rc.d vncserver defaults
